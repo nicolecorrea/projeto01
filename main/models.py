2ce5753ff764c96a2025372779aae36d9c5b58f4
nicolecorrea <na.cg@hotmail.it>
2023-04-17 15:56:00 -0300

    atualizacao02

--- main/models.py
+++ main/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Estudios(models.Model):
 
+    data_preencimento = models.DateField(null=True)
+    nome = models.CharField(max_length = 200)
+    pais = models.CharField(max_length = 200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.nome
